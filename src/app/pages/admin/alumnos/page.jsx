63882ac5e7d04224d8ed6065f6f0f872c831c8fe
victorHerediaa <al22760587@ite.edu.mx>
2024-11-30 13:46:03 -0800

    colores perfiles

--- src/app/pages/admin/alumnos/page.jsx
+++ src/app/pages/admin/alumnos/page.jsx
@@ -141,7 +141,7 @@ export default function Alumnos() {
         <div className="flex flex-col md:flex-row h-screen bg-gray-300 ">
         {/* Formulario */}
         <TravelMenu/>
-        <div className="w-full md:w-2/5 bg-gray-400  text-white p-6 rounded-lg shadow-lg md:sticky md:top-0 h-fit ">
+        <div className="w-full md:w-2/5 bg-gray-400  text-white p-2 rounded-lg shadow-lg md:sticky md:top-0 h-fit ">
 
             <h2 className="text-center text-xl font-bold mb-4">Alumnos</h2>
             <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
